416dcffeda80aaec4cca7f67894419c5d9305d36
Guttesen <og1806x9@hotmail.com>
2018-02-12 23:08:59 +0100

    changed readme

--- mongodb.py
+++ mongodb.py
@@ -4,10 +4,9 @@ from bson.regex import Regex
 import re
 client = MongoClient("mongodb://127.0.0.1:27017")
 db = client.social_net
-#tweet = db.tweets.find({"user": "_TheSpecialOne_"})[0]
 tweet = db.tweets
 
-'''
+
 print("How many Twitter users are in the database?")
 users = tweet.distinct("user")
 print(len(users))
@@ -20,7 +19,7 @@ filters = sorted(filters, key=lambda r: r['text'], reverse=True)
 print(filters[:10])
 
 
-print("Which Twitter users link the most to other Twitter users? (Provide the top ten.)") 
+print("Who is are the most mentioned Twitter users? (Provide the top five.)") 
 pattern = re.compile("(?<=^|(?<=[^a-zA-Z0-9-_\\.]))@([A-Za-z]+[A-Za-z0-9_]+)")
 regex = Regex.from_native(pattern)
 filters = list(db.tweets.aggregate([{"$match": {"text": {"$regex": regex}}},
@@ -28,14 +27,12 @@ filters = list(db.tweets.aggregate([{"$match": {"text": {"$regex": regex}}},
             {"$unwind": "$texts"},{"$match": {"texts": {"$regex": regex}}}, 
             {"$group": {"_id": "$texts", "count": {"$sum": 1}}}]))
 filters = sorted(filters, key=lambda r: r['count'], reverse=True)
-print(filters[:10])
-
+print(filters[:5])
 
 print("Who are the most active Twitter users (top ten)?")
 filters = list(db.tweets.aggregate([{"$group": {"_id": "$user", "count": {"$sum": 1}}}]))
 filters = sorted(filters, key=lambda r: r['count'], reverse=True)
 print(filters[:10])
-'''
 
 print("Who are the five most grumpy (most negative tweets). (Provide five users for each group)")
 filters = list(db.tweets.aggregate([{"$match": {"polarity": {"$eq": 0}}}, {"$group": {"_id": "$user", "count": {"$sum": 1}}}]))
@@ -45,5 +42,3 @@ print("Who are the five the most happy (most positive tweets)?")
 filters = list(db.tweets.aggregate([{"$match": {"polarity": {"$eq": 4}}}, {"$group": {"_id": "$user", "count": {"$sum": 1}}}]))
 filters = sorted(filters, key=lambda r: r['count'], reverse=True)
 print(filters[:5])
-#print("1287 error(s)s")
-
